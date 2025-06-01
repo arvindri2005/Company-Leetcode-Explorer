ec83c1a00f88b406a4c73d2e617fcf30c4eda9f4
Arvind <arvindri2005@gmail.com>
2025-06-02 00:16:42 +0530

    Added new icons

--- next.config.ts
+++ next.config.ts
@@ -24,11 +24,6 @@ const nextConfig: NextConfig = {
             },
         ],
     },
-    experimental: {
-        allowedDevOrigins: [
-            "https://6000-firebase-studio-1748324019627.cluster-htdgsbmflbdmov5xrjithceibm.cloudworkstations.dev",
-        ],
-    },
 };
 
 export default nextConfig;
