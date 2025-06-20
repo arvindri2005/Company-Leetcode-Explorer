81b54297a164d136aaf60afddbf626d7c4da6fa6
Arvind <arvindri2005@gmail.com>
2025-06-21 03:23:32 +0530

    Added adsense tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -127,12 +127,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6342943619826199"
-          crossOrigin="anonymous"
-          strategy="afterInteractive"
-        />
+      <script 
+      async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6342943619826199"
+     crossorigin="anonymous">
+
+     </script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}
