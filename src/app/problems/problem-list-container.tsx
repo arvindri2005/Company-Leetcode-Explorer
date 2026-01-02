e0f3c676c6e9515ad57bd991ee94c57db8b2830f
Arvind <arvindri2005@gmail.com>
2026-01-02 23:29:58 +0530

    feat(seo): add ItemList schema to problems page
    - Implements `ItemList` structured data in `ProblemListContainer` to index the initial list of 50 problems.
    - Uses existing `StructuredData` component for safe JSON-LD injection.
    - Improves semantic understanding of the `/problems` collection for search engines.

--- src/app/problems/problem-list-container.tsx
+++ src/app/problems/problem-list-container.tsx
@@ -1,5 +1,6 @@
 import { problemService } from "@/services/problem.service";
 import AllProblemsList from "@/components/problem/all-problems-list";
+import StructuredData from "@/components/seo/structured-data";
 
 export default async function ProblemListContainer() {
   const { problems, totalPages, currentPage, hasMore, nextCursor } =
@@ -12,21 +13,35 @@ export default async function ProblemListContainer() {
       sortKey: "title",
     });
 
+  const itemListSchema = {
+    "@context": "https://schema.org",
+    "@type": "ItemList",
+    itemListElement: problems.map((problem, index) => ({
+      "@type": "ListItem",
+      position: index + 1,
+      name: problem.title,
+      url: problem.link, // Pointing to external link as we don't have internal detail pages
+    })),
+  };
+
   return (
-    <AllProblemsList
-      initialProblems={problems}
-      itemsPerPage={50}
-      totalPages={totalPages || 1}
-      currentPage={currentPage || 1}
-      hasMore={hasMore}
-      initialNextCursor={nextCursor}
-      initialFilters={{
-        difficultyFilter: [],
-        lastAskedFilter: [],
-        statusFilter: [],
-        searchTerm: "",
-        sortKey: "title",
-      }}
-    />
+    <>
+      <StructuredData data={itemListSchema} />
+      <AllProblemsList
+        initialProblems={problems}
+        itemsPerPage={50}
+        totalPages={totalPages || 1}
+        currentPage={currentPage || 1}
+        hasMore={hasMore}
+        initialNextCursor={nextCursor}
+        initialFilters={{
+          difficultyFilter: [],
+          lastAskedFilter: [],
+          statusFilter: [],
+          searchTerm: "",
+          sortKey: "title",
+        }}
+      />
+    </>
   );
 }
